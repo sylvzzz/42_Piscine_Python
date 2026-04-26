425bf2ba0bcb04aad0e6186d196fa46a6ab8a920
dbotelho <diogolbsilva2006@gmail.com>
2026-04-26 15:55:03 +0100

    Small Corrections

--- PyM10/ex0/lambda_spells.py
+++ PyM10/ex0/lambda_spells.py
@@ -3,20 +3,18 @@ def artifact_sorter(artifacts: list[dict]) -> list[dict]:
 
 
 def power_filter(mages: list[dict], min_power: int) -> list[dict]:
-    return list(filter(lambda m: m['power'] >= min_power, mages))
+    return filter(lambda m: m['power'] >= min_power, mages)
 
 
 def spell_transformer(spells: list[str]) -> list[str]:
-    return list(map(lambda s: f"* {s} *", spells))
+    return map(lambda s: f"* {s} *", spells)
 
 
 def mage_stats(mages: list[dict]) -> dict:
     return {
         'max_power': max(mages, key=lambda m: m['power'])['power'],
         'min_power': min(mages, key=lambda m: m['power'])['power'],
-        'avg_power': round(
-            sum(map(lambda m: m['power'], mages)) / len(mages), 2
-        ),
+        'avg_power': sum(map(lambda m: m['power'], mages)) / len(mages),
     }
 
 
